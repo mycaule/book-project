c47ed54f944fd2a19ae323259e62ca2e6d4481bc
Michel Hua <michel.hua@mfglabs.com>
2017-10-26 16:38:20 +0200

    Build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,7 @@ asciidoctor index.adoc -o output/index.html
 
 # Publish to Github pages
 git checkout gh-pages
-
-git checkout - output/index.html
-echo "in branch gh-pages"
-
-git add output/index.html
-git commit -m "Snapshot on $(date +'%F %T %Z')"
+./publish.sh
 
 # Go back to previous branch
 git checkout -
